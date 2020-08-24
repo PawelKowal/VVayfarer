fc505106f49fb4aef121268520a2c16505abeefa
Paweł Kowal <spadochroniarz424@gmail.com>
2020-08-24 17:59:32 +0200

    Updates

---
+++ precache-manifest.81abe042208a3b5580c56acb91687b2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "459dc6e4d7805ab32ee0575bf806f826",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5a023ccee07be9fc4d9",
+    "url": "/static/css/main.5168d6ac.chunk.css"
+  },
+  {
+    "revision": "506fc58d92b9e04e8ed1",
+    "url": "/static/js/2.e39a4445.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.e39a4445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5a023ccee07be9fc4d9",
+    "url": "/static/js/main.b942e598.chunk.js"
+  },
+  {
+    "revision": "bdeac0278a2b4f470488",
+    "url": "/static/js/runtime-main.48bb6ed6.js"
+  },
+  {
+    "revision": "659df5073a105cffb4afbe478f519b2e",
+    "url": "/static/media/background6.659df507.jpg"
+  }
+]);
